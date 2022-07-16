2b7839b86b316cf7f3eaf396091beea485bffe43
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-15 17:39:52 -0700

    Initial commit. Setup server and static files. Also setup get and post routes.

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+// app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/calc.html');
+});
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const number1 = Number(req.body.num1);
+    const number2 = Number(req.body.num2);
+    const result = number1 + number2;
+    res.send('the result is ' + result)
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('I am awake!')
+});
